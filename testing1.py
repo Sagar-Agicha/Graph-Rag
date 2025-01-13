6e2c7d23286a50321f80cfc188e99945020e7161
admin <admin@localhost.localdomain>
2025-01-13 12:08:31 +0530

    12:08 12/01

--- testing1.py
+++
@@ -1,9 +0,0 @@
-from neo4j import GraphDatabase
-
-
-uri = "bolt://localhost:7687"
-username = "neo4j"
-password = "neo4j"
-driver = GraphDatabase.driver(uri, auth=(username, password))
-driver.verify_connectivity()
-
